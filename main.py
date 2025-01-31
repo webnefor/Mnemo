008d0975da62dda654ec9d3aedd67fce2457353c
Lucas <113185077+webnefor@users.noreply.github.com>
2025-01-31 19:06:10 +0300

    Bug fixed & Upgraded

--- main.py
+++ main.py
@@ -1,33 +1,37 @@
-
-
 import config.settingArgs
 import config.GetWords
 import config.Core
 import config.LabelObject
+import config.MainWindow
+
 import sys
 
 def main() -> int:
 
-    args = config.settingArgs.getparse(sys.argv[1::]);
-
-    config.GetWords.init(args.path);
-
-    kernel_tk = config.Core.CoreTkinter(args.mode);
-
-    kernel_tk.set_style();
-
-    label_objects = config.LabelObject.LabelObject(
-        kernel_tk,config.GetWords.LIST_CONTENTS,
-        config.GetWords.PRONUNCE,
-        config.GetWords.EXAMPLE,
-        config.GetWords.TRANSLATE,
-        args.time
-    );
-
-    label_objects.create();
-
-    kernel_tk.root_window.mainloop();
-
+    window = config.MainWindow.MainWindow();
+    window.setup();
+    window.show();
+    #
+    # args = config.settingArgs.getparse(sys.argv[1::]);
+    #
+    # config.GetWords.init(args.path);
+    #
+    # kernel_tk = config.Core.CoreTkinter(args.mode);
+    #
+    # kernel_tk.set_style();
+    #
+    # label_objects = config.LabelObject.LabelObject(
+    #     kernel_tk,config.GetWords.LIST_CONTENTS,
+    #     config.GetWords.PRONUNCE,
+    #     config.GetWords.EXAMPLE,
+    #     config.GetWords.TRANSLATE,
+    #     args.time
+    # );
+    #
+    # label_objects.create();
+    #
+    # kernel_tk.root_window.mainloop();
+    #
     return 0;
 
 
